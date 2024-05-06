867e5b8451e558be58ffaed40d63e78b7b1057d2
samv-33 <psamuelyl@gmail.com>
2024-05-06 13:26:27 -0700

    made changes, added new files, made navbar

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "@fontsource/roboto"
+import "@fontsource/outfit"
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
