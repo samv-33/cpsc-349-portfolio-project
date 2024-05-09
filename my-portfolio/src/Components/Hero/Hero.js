c37432db26233f700901a27e418efc305ee0bf1b
samv-33 <psamuelyl@gmail.com>
2024-05-09 16:39:16 -0700

    add About and Hero files

--- my-portfolio/src/Components/Hero/Hero.js
+++ my-portfolio/src/Components/Hero/Hero.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { getImageUrl } from '../../utility'
+
+import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
-    <section>
-        <div>
-            <h1>Hi, I'm Sam</h1>
-            <p>I'm a Computer Science student who is 
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi, I'm Sam</h1>
+            <p className={styles.description}>I'm a Computer Science student who is 
                 interested to become a full-stack developer.
-                
+
             </p>
+            <a href="mailto:psamuelyl@gmail.com" className={styles.contactInfo}>Contact Info</a>
         </div>
+        <img src={getImageUrl('assets/hero/myavatar.png')} alt="hero-img" className={styles.heroImg}/>
+        <div className={styles.topBlur}/>
+        <div className={styles.botBlur}/>
     </section>
   )
 }
