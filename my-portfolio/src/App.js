52e20b359675954dfa6ab35ca4c8db370319d4e2
samv-33 <psamuelyl@gmail.com>
2024-05-14 09:18:13 -0700

    added new components and files

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 import About from './Components/About/About';
 import Experience from './Components/Experience/Experience';
+import Projects from './Components/Projects/Projects';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Hero/>
     <About/>
     <Experience/>
+    <Projects/>
   </div>;
 }
 
