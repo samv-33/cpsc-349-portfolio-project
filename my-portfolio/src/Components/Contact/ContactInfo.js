0195268d540b2eda0c3be7be6c989ce53a078dfb
samv-33 <psamuelyl@gmail.com>
2024-05-15 13:10:55 -0700

    updated css

--- my-portfolio/src/Components/Contact/ContactInfo.js
+++ my-portfolio/src/Components/Contact/ContactInfo.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import { getImageUrl } from '../../utility';
+import styles from './Contact.module.css';
 
 const ContactInfo = () => {
-  return <footer>
+  return <footer className={styles.container} id="contact">
+    <div className={styles.text}>
     <h2>Contact Info</h2>
     <p>Any questions? Here are my contact info!
     </p>
-    <ul>
-      <li>
+    </div>
+    <ul className={styles.links}>
+      <li className={styles.link}>
         <img src={getImageUrl("assets/contacts/gmailIcon.png")} alt="email contact info"/>
-        <p></p>
+        <a href="mailto:psamuelyl@gmail.com">psamuelyl@gmail.com</a>
 
       </li>
-      <li>
+      <li className={styles.link}>
         <img src={getImageUrl("assets/contacts/githubIcon.png")} alt="github contact info"/>
-        <p></p>
+        <a href="https://www.github.com/samv-33" target="_blank" rel="noopener noreferrer">github.com/samv-33</a>
 
       </li>
     </ul>
